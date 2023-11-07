96aecaf2f7bb97cfd1e09949be1020e91f5577c6
nlunce <naterrl15@byui.edu>
2023-11-07 16:06:13 -0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from "./Header/Header";
+export { default as CoreConcept } from "./CoreConcept/CoreConcept";
+export { default as TabButton } from "./TabButton/TabButton";
