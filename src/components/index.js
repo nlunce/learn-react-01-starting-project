1dc3c55241450b4689477236d74b4b3649ca18bc
nlunce <naterrl15@byui.edu>
2023-11-07 17:08:32 -0700

    restruct

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as CoreConcept } from "./CoreConcept/CoreConcept";
+export { default as CoreConcepts } from "./CoreConcepts/CoreConcepts";
+export { default as Examples } from "./Examples/Examples";
 export { default as TabButton } from "./TabButton/TabButton";
